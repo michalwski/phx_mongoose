596d412d974afd56b32ffdc4b4941b69e0fd4c9a
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2015-01-09 15:16:31 +0100

    adds mongoose as a dep
    overrides some deps used in many places

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule PhxMongoose.Mixfile do
   # Type `mix help compile.app` for more information
   def application do
     [mod: {PhxMongoose, []},
-     applications: [:phoenix, :cowboy, :logger]]
+     applications: [:phoenix, :cowboy, :logger, :mongoose]]
   end
 
   # Specifies your project dependencies
@@ -23,6 +23,14 @@ defmodule PhxMongoose.Mixfile do
   # Type `mix help deps` for examples and options
   defp deps do
     [{:phoenix, "~> 0.7.2"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0", override: true},
+     {:mongooseim, git: "https://github.com/esl/MongooseIM.git", branch: "no-rel-required"},
+      # overrides
+     {:proper,  git: "git://github.com/manopapad/proper.git", tag: "v1.1", override: true},
+     {:p1_utils, git: "git://github.com/processone/p1_utils", ref: "9e646e4", override: true},
+     {:meck, git: "git://github.com/eproxus/meck.git", tag: "0.8.2", override: true},
+     {:folsom, git: "git://github.com/boundary/folsom.git", ref: "4824aec", override: true}
+    ]
+
   end
 end
