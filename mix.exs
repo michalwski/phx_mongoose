8a3ce88fc2135b87aeda956c7c95242afc44e47a
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2015-01-13 14:42:55 +0100

    update phoenix to 0.8.0

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule PhxMongoose.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 0.7.2"},
+    [{:phoenix, "~> 0.8.0"},
      {:cowboy, "~> 1.0", override: true},
      {:mongooseim, git: "https://github.com/esl/MongooseIM.git", branch: "phoenix-integration"},
       # overrides
